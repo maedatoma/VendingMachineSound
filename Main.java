869741b4ca9232b9e41ca5fb90d26a538790fff3
前田 透馬 <160041@yses.ac.jp>
2017-07-06 11:54:04 +0900

    Main.java added method

--- Main.java
+++ Main.java
@@ -4,10 +4,22 @@ public class Main{
 		InterfaceMan ifm = new InterfaceMan();
 
 		// Quizman インスタンスの実行
-		Quizman qm = new Quizman();
+		QuizMan qm = new QuizMan();
 
 		// メソッドを実行
-		ifm.start();
-		qm.answer();
+			// メッセージを出力させて、回答を保存させる
+			ifm.show_Msg_Input();
+
+			// 回答を受け取って、結果を判定する
+			int get_Answer = ifm.get_User_Answer();
+
+			// 正解の場合、 InterFaceMan に正解を表示
+			if( qm.judge( get_Answer ) ){
+				ifm.show_Msg_Ok();
+
+			// そうでない場合
+			} else {
+				ifm.show_Msg_No();
+			}
 	}
 }
