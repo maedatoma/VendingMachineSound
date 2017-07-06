9cc153be11bca028b5938d59ee53b89f98f34921
前田 透馬 <160041@yses.ac.jp>
2017-07-06 10:45:43 +0900

    fixed Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,13 @@
 public class Main{
 	public static void main(String[] args) {
-
 		// Interface インスタンスの生成
 		InterfaceMan ifm = new InterfaceMan();
 
 		// Quizman インスタンスの実行
-		//Quizman qm = new Quizman();
+		Quizman qm = new Quizman();
 
-		// 実行
+		// メソッドを実行
 		ifm.start();
+		qm.answer();
 	}
 }
