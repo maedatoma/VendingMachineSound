742ddc36d9e6c6f691f840df35d79ff01fece6da
前田 透馬 <160041@yses.ac.jp>
2017-07-06 11:55:29 +0900

    InterfaceMan.javaに処理を追加

--- InterfaceMan.java
+++ InterfaceMan.java
@@ -1,16 +1,32 @@
 class InterfaceMan{
 	// data
-	String msg1 = "数を入力してください >";
-	String msg2 = "が入力されました";
-	int num;
+	String msg1 = "数値を入力してください >";
+	String msg2 = "正解";
+	String msg3 = "不正解";
+	int num_User;	// 入力した数値
 
 	// method
-	public void start(){
+	// 入力を促す処理
+	public void show_Msg_Input(){
 		// 入力を促す
 		System.out.println( msg1 );
-		num = new java.util.Scanner(System.in).nextInt();
 
-		// 入力内容を表示
-		System.out.println( num + msg2 );
+		// 数値を入力する
+		num_User = new java.util.Scanner(System.in).nextInt();
+	}
+
+	// 入力した回答を返す処理
+	public int get_User_Answer(){
+		return num_User;
+	}
+
+	// 正解を出力する処理
+	public void show_Msg_Ok(){
+		System.out.println( msg2 );
+	}
+
+	// 不正解を出力する処理
+	public void show_Msg_No(){
+		System.out.println( msg3 );
 	}
 }
