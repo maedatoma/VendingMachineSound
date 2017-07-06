022fcfb63775823488409777f4dc8be826167d81
前田 透馬 <160041@yses.ac.jp>
2017-07-06 10:44:28 +0900

    added QuizMan.java

---
+++ Quizman.java
@@ -0,0 +1,22 @@
+class Quizman{
+	// data
+	int num_Ans = 1;
+	String msg1 = "正解！";
+	String msg2 = "ハズレ";
+
+	// method
+	public void answer(){
+		System.out.println("test");
+
+		/*
+		// 入力内容と正解の数が一致していたら
+		if( num == num_Ans ){
+			System.out.println( msg1 );
+
+		// そうでない場合は
+		} else {
+			System.out.println( msg2 );
+		}
+		*/
+	}
+}
