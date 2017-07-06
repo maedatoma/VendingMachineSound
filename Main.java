5c7f95aa60ac619cecb686ab9b4736d0e22c2696
前田 透馬 <160041@yses.ac.jp>
2017-07-06 10:07:00 +0900

    added Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main{
+	public static void main(String[] args) {
+
+		// Interface インスタンスの生成
+		InterfaceMan ifm = new InterfaceMan();
+
+		// Quizman インスタンスの実行
+		//Quizman qm = new Quizman();
+
+		// 実行
+		ifm.start();
+	}
+}
